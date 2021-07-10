dfc0c41d80a0f30f6dea2fd0bbdea8d56971d3d0
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-07-10 09:34:26 +0200

    Add task list component & stories

---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Task } from '../models/task.model';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent implements OnInit {
+
+  tasksInOrder: Task[] = [];
+
+  @Input()
+    set tasks(arr: Task[]) {
+      this.tasksInOrder = [
+        ...arr.filter(t => t.state === 'TASK_PINNED'),
+        ...arr.filter(t => t.state !== 'TASK_PINNED'),
+      ];
+    }
+
+  @Input() loading = false;
+
+  @Output()
+  onPinTask = new EventEmitter<Event>();
+
+  @Output()
+  onArchiveTask = new EventEmitter<Event>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
