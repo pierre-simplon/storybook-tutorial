750df12b1fb477f161d21aef13987f239a4ee25d
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-07-11 10:17:51 +0200

    Add tasklist tests

--- src/app/task-list/task-list.component.spec.ts
+++ src/app/task-list/task-list.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { render } from '@testing-library/angular';
+import { TaskComponent } from '../task/task.component';
 
 import { TaskListComponent } from './task-list.component';
+import { WithPinnedTasks } from './task-list.stories';
 
 describe('TaskListComponent', () => {
   let component: TaskListComponent;
@@ -21,5 +24,25 @@ describe('TaskListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+});
+
+describe('TaskList component', () => {
+  it('renders pinned tasks at the start of the list', async () => {
+    const mockedActions = jest.fn();
+    const tree = await render(TaskListComponent, {
+      declarations: [TaskComponent],
+      componentProperties: {
+        ...WithPinnedTasks.args,
+        onPinTask: {
+          emit: mockedActions,
+        } as any,
+        onArchiveTask: {
+          emit: mockedActions,
+        } as any,
+      },
+    });
+    const component = tree.fixture.componentInstance;
+    expect(component.tasksInOrder[0].id).toBe('6');
   });
 });
