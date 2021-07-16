e89366d072fc5dbcc2d2efa47397efaa80d3e912
pierre-simplon <pierre.jolivet.devweb@gmail.com>
2021-07-16 07:59:38 +0200

    Update Jest test

--- src/app/task-list/task-list.component.spec.ts
+++ src/app/task-list/task-list.component.spec.ts
@@ -1,36 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { render } from '@testing-library/angular';
 import { TaskComponent } from '../task/task.component';
+import { PureTaskListComponent } from './pure-task-list.component';
 
-import { TaskListComponent } from './task-list.component';
-import { WithPinnedTasks } from './task-list.stories';
 
-describe('TaskListComponent', () => {
-  let component: TaskListComponent;
-  let fixture: ComponentFixture<TaskListComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TaskListComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TaskListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+import { WithPinnedTasks } from './pure-task-list.stories';
 
 describe('TaskList component', () => {
   it('renders pinned tasks at the start of the list', async () => {
     const mockedActions = jest.fn();
-    const tree = await render(TaskListComponent, {
+    const tree = await render(PureTaskListComponent, {
       declarations: [TaskComponent],
       componentProperties: {
         ...WithPinnedTasks.args,
